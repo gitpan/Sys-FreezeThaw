6e5005d585a1aeb3c9db2924c4ac9f3509daf734
Marc Lehmann <schmorp@schmorp.de>
2013-10-07 04:21:35 +0000

    Import of MLEHMANN/Sys-FreezeThaw-0.02 from CPAN.
    gitpan-cpan-distribution: Sys-FreezeThaw
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MLEHMANN/Sys-FreezeThaw-0.02.tar.gz
    gitpan-cpan-author:       MLEHMANN
    gitpan-cpan-maturity:     released

--- FreezeThaw.pm
+++ FreezeThaw.pm
@@ -16,16 +16,23 @@ Sys::FreezeThaw - stop and start all user processes on a machine
   
 =head1 DESCRIPTION
 
-Operating Systems current supported: Linux-2.6.
+Operating Systems/Kernels current supported: Linux-2.6/3.0 with F</proc>.
 
 This module implements a very specific feature: stopping(freezing and
-thawing/continuing all user processes on the machine. It works by sending
-SIGSTOP to all processes, parent-process first, so that the wait syscall
-will not trigger on stopped children. Restarting is done in reverse order.
+thawing/continuing all userspace processes on the machine. It works by
+sending SIGSTOP to all processes, parent-process first, so that the wait
+syscall will not trigger on stopped children. Restarting is done in
+reverse order.
 
 Using the combined function Sys::FreezeThaw::freezethaw is recommended as
 it will catch runtime errors, but stopping and restarting can be dine via
-seperate function calls.
+separate function calls.
+
+=head2 What could it possibly be sueful for??
+
+Possible uses include: doing atomic file system operations (such as
+replacing files while they are guaranteed not to be in use), or quieting
+down a system to investigate suspicious behaviour.
 
 =over 4
 
@@ -35,31 +42,32 @@ package Sys::FreezeThaw;
 
 use Carp;
 
-$VERSION = '0.01';
+$VERSION = '0.02';
+$PARTIAL_OK = 0;
 
 =item Sys::FreezeThaw::freezethaw { BLOCK }
 
-First tries to stop all processes. If successful, runs the given codeblock
+First tries to stop all processes. If successful, runs the given code block
 (or code reference), then restarts all processes again. As the system is
-basically frozen during the codeblock execution, it should be as fast as
+basically frozen during the code block execution, it should be as fast as
 possible.
 
 Runtime errors will be caught with C<eval>. If an exception occurs it will
 be re-thrown after processes are restarted. If processes cannot be frozen
 or restarted, this function will throw an exception.
 
-Signal handlers for SIGPIPE, SIGHUP, SIGALRM, SIGUSR1 and SIGUSR2 will
-temporarily be installed, so if you want to catch these, you have to do so
-yourself within the executed codeblock.
+Signal handlers for SIGINT, SIGTERM, SIGPIPE, SIGHUP, SIGALRM, SIGUSR1 and
+SIGUSR2 will be installed temporarily, so if you want to catch these, you
+have to do so yourself within the executed code block.
 
 Try to do as few things as possible. For example, outputting text might
 cause a deadlock, as the terminal emulator on the other side of STDOUT
-might be stopped, etc.
+might be stopped, logging to syslog might not work and so on.
 
-The return value of the codeblock is ignored right now, and the function
+The return value of the code block is ignored right now, and the function
 doesn't yet return anything sensible.
 
-=item $token = Sys::FreezeThaw::freeze;
+=item $token = Sys::FreezeThaw::freeze
 
 Send SIGSTOP to all processes, and return a token that allows them to be
 thawed again.
@@ -70,8 +78,15 @@ will automatically be thawed.
 =item Sys::FreezeThaw::thaw $token
 
 Take a token returned by Sys::FreezeThaw::freeze and send all processes
-a CONT signal, in the order required for them not to receive child STOP
+a C<CONT> signal, in the order required for them not to receive child STOP
 notifications.
+
+=item $Sys::FreezeThaw::PARTIAL_OK
+
+A boolean that tells C<freeze> whether it is an error if a process cannot
+be stopped. If false (the default), then C<freeze> will fail if there is
+an unstoppable process. If it is true, then C<freeze> will pretend it the
+process stopped.
 
 =cut
 
@@ -82,8 +97,10 @@ sub yield {
 
 # the maximum number of iterations per stop/cont etc. loop
 # used to shield against catastrophic events (or bugs :)
-# usually only one or two iterations are required per loop
-sub MAX_WAIT() { 100 }
+# on current linux systems it can take an enourmous amount of
+# time for some processes to stop, but usually it only takes
+# one or two iterations.
+sub MAX_WAIT() { 10 }
 
 # return a list o fall pid's in the system,
 # topologically sorted parent-first
@@ -157,10 +174,10 @@ sub thaw($) {
    }
 }
 
-sub freeze() {
+sub freeze(;$) {
    local $@;
 
-   my @procs;
+   my $procs;
 
    eval {
       for (1..MAX_WAIT) {
@@ -180,7 +197,7 @@ sub freeze() {
                yield;
             }
 
-            die "unable to stop processes <@pids>" if @pids;
+            die "unable to stop some processes: @pids" if @pids && !$PARTIAL_OK;
          }
       }
    };
@@ -200,6 +217,8 @@ sub freezethaw(&) {
 
    eval {
       local $SIG{HUP}  = sub { die "ERROR: caught SIGHUP while system frozen" };
+      local $SIG{INT}  = sub { die "ERROR: caught SIGINT while system frozen" };
+      local $SIG{TERM} = sub { die "ERROR: caught SIGTERM while system frozen" };
       local $SIG{PIPE} = sub { die "ERROR: caught SIGPIPE while system frozen" };
       local $SIG{ALRM} = sub { die "ERROR: caught SIGALRM while system frozen" };
       local $SIG{USR1} = sub { die "ERROR: caught SIGUSR1 while system frozen" };
